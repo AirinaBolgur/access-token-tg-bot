71bd0305d90d264bdacc8292ef2951b489413d1f
nessshon <shonness98@gmail.com>
2024-11-08 13:23:10 +0500

    Update texts_pics.

--- app/texts_pics.py
+++ app/texts_pics.py
@@ -1,7 +1,7 @@
 pictures = {
-    "Main": "https://telegra.ph//file/aaba319da09f60e6def03.jpg",
-    "Welcome": "https://telegra.ph//file/db9c5c3febe75811e41af.jpg",
-    "Access denied": "https://telegra.ph//file/ceec89ba75c903210411c.jpg",
-    "Access granted": "https://telegra.ph//file/6b03c59182d959cddeb02.jpg",
-    "Connect": "https://telegra.ph//file/a4ddc111ff41692ad5200.jpg"
+    "Main": "https://i.ibb.co/k4ZHKVv/a-a-11-08-04-52-15.jpg",
+    "Welcome": "https://i.ibb.co/k4ZHKVv/a-a-11-08-04-52-15.jpg",
+    "Access denied": "https://i.ibb.co/k4ZHKVv/a-a-11-08-04-52-15.jpg",
+    "Access granted": "https://i.ibb.co/k4ZHKVv/a-a-11-08-04-52-15.jpg",
+    "Connect": "https://i.ibb.co/k4ZHKVv/a-a-11-08-04-52-15.jpg"
 }
